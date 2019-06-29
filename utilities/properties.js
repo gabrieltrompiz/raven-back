0cbc7022cb4d0109bab896e2a167753527eb80e8
Luis Petrella <aax_00ap@hotmail.com>
2019-06-28 21:19:30 -0400

    Added conversation and messages queries to properties
    And updated db.txt (deleted/renamed columns)

--- utilities/properties.js
+++ utilities/properties.js
@@ -11,17 +11,35 @@ module.exports = {
     updatePassword: 'UPDATE users SET user_password = $1 WHERE user_id = $2;',
     updatePhone: 'UPDATE users SET user_phone = $1 WHERE user_id = $2;',
     updatePicture: 'UPDATE users SET user_picture_url = $1 WHERE user_id = $2;',
-    deleteUser: 'DELETE FROM users WHERE user_id = $1',
+    deleteUser: 'DELETE FROM users WHERE user_id = $1;',
 
     //Status Queries
-    getStatusList: 'SELECT status_id, status_description, is_active FROM status WHERE user_id = $1',
-    uploadStatus: 'UPDATE FROM status SET is_active = FALSE WHERE status_id = $1;' +
-        'INSERT INTO status (user_id, status_description) VALUES ($2, \'$3\');'
-    updateStatus: 'UPDATE FROM status SET is_active = FALSE WHERE status_id = $1;' +
-        'UPDATE FROM status SET is_active = TRUE WHERE status_id = $2;',
-    deleteStatus: 'DO $do$ BEGIN IF EXISTS (SELECT * FROM status WHERE status_id = $1 AND is_active = FALSE)' +
-        'THEN DELETE FROM status WHERE status_id = $1; END IF; END $do$'
+    getStatusList: 'SELECT status_id, status_description, is_active FROM status WHERE user_id = $1;',
+    uploadStatus: 'UPDATE status SET is_active = FALSE WHERE status_id = $1;' +
+        'INSERT INTO status (user_id, status_description) VALUES ($2, \'$3\');',
+    updateStatus: 'UPDATE status SET is_active = FALSE WHERE status_id = $1;' +
+        'UPDATE status SET is_active = TRUE WHERE status_id = $2;',
+    deleteStatus: 'DO $do$ BEGIN IF EXISTS (SELECT * FROM status WHERE status_id = $1 AND is_active = FALSE);' +
+        'THEN DELETE FROM status WHERE status_id = $1; END IF; END $do$',
 
     //Conversation Queries
-    createConversation: ''
+    createConversation: 'INSERT INTO conversation (type_conversation_id, creator_id, conversation_name) VALUES ($1, $2, $3);',
+    insertUserToConversation: 'INSERT INTO conversation_users (user_id, type_user_id, conversation_id) VALUES ($1, $2, $3);',
+    removeUserFromConversation: 'DELETE FROM conversation_users WHERE conversation_user_id = $1;',
+    getConversationList: 'SELECT cu.type_user_id, co.* FROM conversation_users cu INNER JOIN conversation co ' +
+        'ON cu.conversation_id = co.conversation_id WHERE cu.user_id = $1;',
+    searchConversation: 'SELECT cu.type_user_id, co.* FROM conversation_users cu INNER JOIN conversation co ' +
+        'ON cu.conversation_id = co.conversation_id WHERE cu.user_id = $1 AND co.conversation_name ILIKE $2;',
+    updateConversationName: 'UPDATE conversation SET conversation_name = $1 WHERE conversation_id = $2;',
+    updateTypeUser: 'UPDATE conversation_users SET type_user_id = $1 WHERE conversation_user_id = $2;',
+    deleteConversation: 'DELETE FROM conversation_users WHERE conversation_id = $1;' +
+        'DELETE FROM message WHERE conversation_id = $1;' +
+        'DELETE FROM conversation WHERE conversation_id = $1;',
+
+    //Message Queries
+    insertMessage: 'INSERT INTO message (user_id, conversation_id, message_attachment, message_body);',
+    getMessageList: 'SELECT message_id, user_id, message_attachment, message_body, message_creation_date FROM message WHERE conversation_id = $1;',
+    searchMessages: 'SELECT message_id, user_id, message_attachment, message_body, message_creation_date ' +
+        'FROM message WHERE conversation_id = $1 AND message_body ILIKE \'%\'$2\'%\';',
+    deleteMessage: 'DELETE FROM messages WHERE message_id = $1;'
 };
