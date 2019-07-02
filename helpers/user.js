c79e0adf665c6f817d0c98eeef6700b6b7a39735
Luis Petrella <aax_00ap@hotmail.com>
2019-07-01 21:04:02 -0400

    updated database and fixed a register bug

--- helpers/user.js
+++ helpers/user.js
@@ -37,7 +37,7 @@ module.exports.getUserByPhone = phone => {
 module.exports.register = user => {
     return new Promise((res, rej) => {
         db.connect().then(obj => {
-            obj.one(properties.registerUser, [user.phone, user.name, user.email, '../assets/default.jpg', user.password]).then(user => {
+            obj.one(properties.registerUser, [user.name, user.email, '../assets/default.jpg', user.password]).then(user => {
                 res(user);
                 obj.done();
             });
